83f603c3a1bdc7734a679261a5eefd0b0cd95be6
Deol Satish <164171952+deol-satish@users.noreply.github.com>
2024-08-06 17:56:50 +1000

    dasd

--- New Setup/test_copy_confs.sh
+++ New Setup/test_copy_confs.sh
@@ -5,26 +5,26 @@
 # Assumes private/public keys have been copied already, is executed on the
 # VM Host machine
 
-scp -P 3322 -p -i ~/.ssh/mptcprootkey confs/client1-confs/rc.conf root@192.168.56.1:/etc/
-scp -P 3322 -p -i ~/.ssh/mptcprootkey confs/client1-confs/ipfw.rules root@192.168.56.1:/etc/
-scp -P 3322 -p -i ~/.ssh/mptcprootkey confs/client1-confs/loader.conf root@192.168.56.1:/boot/
+scp -P 3322 -p -i ~/.ssh/mptcprootkey confs/test1-confs/rc.conf root@192.168.56.1:/etc/
+scp -P 3322 -p -i ~/.ssh/mptcprootkey confs/test1-confs/ipfw.rules root@192.168.56.1:/etc/
+scp -P 3322 -p -i ~/.ssh/mptcprootkey confs/test1-confs/loader.conf root@192.168.56.1:/boot/
 
 
 
-scp -P 3323 -p -i ~/.ssh/mptcprootkey confs/client2-confs/rc.conf root@192.168.56.1:/etc/
-scp -P 3323 -p -i ~/.ssh/mptcprootkey confs/client2-confs/ipfw.rules root@192.168.56.1:/etc/
-scp -P 3323 -p -i ~/.ssh/mptcprootkey confs/client2-confs/loader.conf root@192.168.56.1:/boot/
+scp -P 3323 -p -i ~/.ssh/mptcprootkey confs/test2-confs/rc.conf root@192.168.56.1:/etc/
+scp -P 3323 -p -i ~/.ssh/mptcprootkey confs/test2-confs/ipfw.rules root@192.168.56.1:/etc/
+scp -P 3323 -p -i ~/.ssh/mptcprootkey confs/test2-confs/loader.conf root@192.168.56.1:/boot/
 
 
 
-scp -P 4422 -p -i ~/.ssh/mptcprootkey confs/router-confs/rc.conf root@192.168.56.1:/etc/
-scp -P 4422 -p -i ~/.ssh/mptcprootkey confs/router-confs/ipfw.rules root@192.168.56.1:/etc/
-scp -P 4422 -p -i ~/.ssh/mptcprootkey confs/router-confs/loader.conf root@192.168.56.1:/boot/
+scp -P 4422 -p -i ~/.ssh/mptcprootkey confs/DummynetVM1-confs/rc.conf root@192.168.56.1:/etc/
+scp -P 4422 -p -i ~/.ssh/mptcprootkey confs/DummynetVM1-confs/ipfw.rules root@192.168.56.1:/etc/
+scp -P 4422 -p -i ~/.ssh/mptcprootkey confs/DummynetVM1-confs/loader.conf root@192.168.56.1:/boot/
 
 
-scp -P 4423 -p -i ~/.ssh/mptcprootkey confs/server-confs/rc.conf root@192.168.56.1:/etc/
-scp -P 4423 -p -i ~/.ssh/mptcprootkey confs/server-confs/ipfw.rules root@192.168.56.1:/etc/
-scp -P 4423 -p -i ~/.ssh/mptcprootkey confs/server-confs/loader.conf root@192.168.56.1:/boot/
+scp -P 4423 -p -i ~/.ssh/mptcprootkey confs/DummynetVM2-confs/rc.conf root@192.168.56.1:/etc/
+scp -P 4423 -p -i ~/.ssh/mptcprootkey confs/DummynetVM2-confs/ipfw.rules root@192.168.56.1:/etc/
+scp -P 4423 -p -i ~/.ssh/mptcprootkey confs/DummynetVM2-confs/loader.conf root@192.168.56.1:/boot/
 
 
 
